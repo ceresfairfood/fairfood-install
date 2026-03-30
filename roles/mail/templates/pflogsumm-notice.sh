b0ce078ac99adf522c9262aef08c4c17f58c7479
David Cook <david@redcliffs.net>
2026-03-30 15:54:51 +1100

    Email postfix log summary if any errors

---
+++ roles/mail/templates/pflogsumm-notice.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e # Exit if any command fails
+# Check if there are any bounce/failure/error messages that might be worth knowing about
+# and email the report to admins.
+
+tmpfile=/tmp/pflogsumm-notice.txt
+to_address=postmaster
+
+pflogsumm -d yesterday -u 0 --problems-first --verbose-msg-detail /var/log/mail.log > $tmpfile
+
+if egrep -m1 -q "(by relay\)|(reject|hold|discard) detail|failures|Warnings|Errors|Panics)$" $tmpfile; then
+  mailx -s "Email error stats: `uname -n`" $to_address < $tmpfile
+fi
